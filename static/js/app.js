1d02c3978c476f6f55374bcc5cb39613061742fd
Maciej Sartys <sartys.maciej@gmail.com>
2016-06-24 23:31:16 +0000

    Universal Api query function

--- static/js/app.js
+++ static/js/app.js
@@ -12,8 +12,31 @@
             }
         }
 
+        var queryApi = function(uri, err, success) {
+            // build and sent async get request to api
+            // err, success - callback functions for request
+
+            var request = new XMLHttpRequest();
+
+            request.onreadystatechange = function() {
+                if (request.readyState === 4) {
+                    if (request.status >= 200 || request.status < 400) {
+                        success(request);
+                    }
+                    else {
+                        err(request);
+                    }
+                }
+            };
+
+            request.open('GET', 'https://api.twitch.tv/kraken/' + uri, true);
+
+            request.setRequestHeader('Accept', 'application/vnd.twitchtv.v3+json');
+            request.send();
+        };
+
         var renderList = function() {
-            
+
             var streamer = function(name, activity, status, icon) {
                 this.name = name;
                 this.activity = activity;
@@ -21,11 +44,13 @@
                 this.status = status;
             };
 
+
             var streamersList = [
-                new streamer("ESL_SC2", 'RERUN: StarCraft 2 - LiquidBunny vs. Petraeus (TvZ) - WCS Premier League Season 2 2015 - Ro32 Group A', 
-            'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg'),
-            new streamer("freecodecamp", 'freecodecamp', 'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg'),
-            new streamer("storbeck", 'storbeck', 'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg')];
+                new streamer("ESL_SC2", 'RERUN: StarCraft 2 - LiquidBunny vs. Petraeus (TvZ) - WCS Premier League Season 2 2015 - Ro32 Group A',
+                    'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg'),
+                new streamer("freecodecamp", 'freecodecamp', 'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg'),
+                new streamer("storbeck", 'storbeck', 'online', 'https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-300x300.jpeg')
+            ];
 
             var listHtml = document.getElementById('streamers');
 
