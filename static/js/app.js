fd68183b901fb617d644cfd5b613bf71f9eb8d93
Maciej Sartys <sartys.maciej@gmail.com>
2016-07-06 13:06:13 +0000

    Main channel rendering separated from other channels

--- static/js/app.js
+++ static/js/app.js
@@ -89,28 +89,53 @@
 
     var View = function(scope) {
         this.scope = scope;
-        this.mainChannel = document.getElementsByClassName('main-channel').item(0);
+        this.mainChannel = {};
+        this.mainChannel['description'] = document.getElementsByClassName('main-channel-description').item(0);
         this.otherChannels = document.getElementsByClassName('channels').item(0);
         this.errorDiv = document.getElementById('error');
     };
     
     View.prototype.renderPage = function () {
+        var self = this;
         
+        var mainChannel = self.scope.channelsData.filter(function(channel) {
+            return channel.name === 'freecodecamp';
+        }, self);
+        
+        var otherChannels = self.scope.channelsData.filter(function(channel) {
+            return channel.name !== 'freecodecamp';
+        }, self);
+        
+        self.updateMainChannel(mainChannel.pop());
+        self.renderList(otherChannels);
     };
     
-    View.prototype.updateMainChannel = function() {
+    /**
+     * @param {Streamer Object} Freecodecamp channel data
+     */
+    View.prototype.updateMainChannel = function(channel) {
+        var self = this;
         
+        self.mainChannel.description.innerHTML = channel.activity;
     };
+    
+    /**
+     * Render list of Streamers with online channels on top
+     * 
+     * @param {Array<Object>} Streamers
+     */
 
-    View.prototype.renderList = function() {
+    View.prototype.renderList = function(channels) {
+
+        
         var self = this;
         
         self.otherChannels.innerHTML = '';
         
         //online channels first
-        var listHTML = self.scope.channelsData.filter(function(element) {
+        var listHTML = channels.filter(function(element) {
             return element.status === 'online';
-        }).concat(self.scope.channelsData.filter(function(element) {
+        }).concat(channels.filter(function(element) {
             return element.status === 'offline';
         })).map(function(element) {
             return this.channelToDOM(element.name, element.activity, element.icon);
@@ -167,7 +192,8 @@
     };
 
     App.prototype.start = function() {
-        this.getChannelsData(this.scope.channelsNames, this.view.renderList.bind(this.view));
+        var self = this;
+        self.getChannelsData(self.scope.channelsNames, self.view.renderPage.bind(self.view));
     };
 
     var app = new App;
