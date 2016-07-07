c928c56104a1b60688841db834f010085107acf3
Maciej Sartys <sartys.maciej@gmail.com>
2016-07-07 08:17:23 +0000

    js code moved to app/frontend into separated files

---
+++ app/frontend/streamer.js
@@ -0,0 +1,18 @@
+//-----------------------------------------------------------------------------
+
+/**
+ * Streamer Object
+ * 
+ * Container for a streamer data to be dislayed 
+ * 
+ * @param {string} Username [GET /channels/:channel/] => display_name
+ * @param {string} Current user activity on channel - [GET /channels/:channel/] => status
+ * @param {string} Online/offline flag based on [GET /streams/:channel/] => stream
+ * @param {string} Channel logo url - [GET /channels/:channel/] => logo
+ */
+var Streamer = function(name, activity, status, icon) {
+    this.name = name;
+    this.activity = activity;
+    this.icon = icon;
+    this.status = status;
+};
